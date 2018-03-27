eb3d446644376645b2783710d86a09353c0a5ea0
Unknown <pedrodlafuente@gmail.com>
2018-03-27 14:04:35 +0200

    Appcontroller added

---
+++ PjParking/src/com/pjmorgan/parking/controller/AppController.java
@@ -0,0 +1,13 @@
+package com.pjmorgan.parking.controller;
+
+public interface AppController {
+
+	public void inicio();
+	public void nuevaEntrada();
+	public void nuevaSalida();
+	public void entradaOk();
+	public void entradaFail();
+	public void salidaOk();
+	public void salidaFail();
+	
+}
---
+++ PjParking/src/com/pjmorgan/parking/controller/impl/AppControllerImpl.java
@@ -0,0 +1,49 @@
+package com.pjmorgan.parking.controller.impl;
+
+import com.pjmorgan.parking.controller.AppController;
+
+public class AppControllerImpl implements AppController {
+
+	@Override
+	public void inicio() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void nuevaEntrada() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void nuevaSalida() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void entradaOk() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void entradaFail() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void salidaOk() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void salidaFail() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
