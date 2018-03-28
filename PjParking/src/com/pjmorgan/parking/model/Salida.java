a746463d9f5e6d486eb6b0c0bdf7527c308a5b68
Unknown <jacobomillan_87@hotmail.com>
2018-03-28 17:02:57 +0200

    Salida defined

--- PjParking/src/com/pjmorgan/parking/model/Salida.java
+++ PjParking/src/com/pjmorgan/parking/model/Salida.java
@@ -2,4 +2,38 @@ package com.pjmorgan.parking.model;
 
 public class Salida {
 
+	
+	private String matricula;
+	private double horas;
+	
+	public Salida(String matricula, double horas) {
+		super();
+		this.matricula = matricula;
+		this.horas = horas;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public double getHoras() {
+		return horas;
+	}
+
+	public void setHoras(double horas) {
+		this.horas = horas;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
